6a39cda0ab195b9feced1f4c5608dec1d1cfbc3b
Ojashwee <166322643+Ojashwee07@users.noreply.github.com>
2026-04-06 18:37:51 +0530

    Delete agent.py

--- agent.py
+++
@@ -1,8 +0,0 @@
-import numpy as np,random
-class QLearningAgent:
-    def __init__(self):
-        self.q=np.zeros((3,4));self.lr=0.1;self.gamma=0.9;self.eps=0.2
-    def act(self,s):
-        return random.randint(0,3) if random.random()<self.eps else int(self.q[s].argmax())
-    def update(self,s,a,r,ns):
-        self.q[s][a]+=self.lr*(r+self.gamma*self.q[ns].max()-self.q[s][a])
